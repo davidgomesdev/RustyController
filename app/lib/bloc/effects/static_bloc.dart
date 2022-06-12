2732823f9b9be8b028700ef3a2dd878198b9777a
David Gomes <LegendL3n@users.noreply.github.com>
2022-06-12 17:16:12 +0100

    Fixed static effect not sending request

--- app/lib/bloc/effects/static_bloc.dart
+++ app/lib/bloc/effects/static_bloc.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rusty_controller/bloc/specific_effect_bloc.dart';
 import 'package:rusty_controller/model/led_effects.dart';
 
-class StaticBloc extends Bloc<StaticEffectEvent, StaticEffect> {
+class StaticBloc extends SpecificEffectBloc<StaticEffectEvent, StaticEffect> {
   StaticBloc(StaticEffect effect) : super(effect) {
     on<StaticColorEvent>(
         (event, emit) => emit(state..color = event.currentColor));
