4e309dd9c3e0772919f7478463336ae65df071c0
David Gomes <LegendL3n@users.noreply.github.com>
2022-09-16 17:22:55 +0100

    [Feature] - App: Save effect settings (#12)
    * Fixed Windows app throwing MissingPluginException on discovery
    
    * App: Added store service to save effect settings

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -12,8 +12,9 @@ import 'package:rusty_controller/model/led_effects.dart';
 import 'package:rusty_controller/screen/home_screen.dart';
 import 'package:rusty_controller/service/controller_service.dart';
 import 'package:rusty_controller/service/discovery_service.dart';
+import 'package:rusty_controller/service/store_service.dart';
 
-var log = Logger(level: Level.debug, printer: PrettyPrinter());
+var log = Logger(level: Level.verbose, printer: PrettyPrinter());
 var serviceLocator = GetIt.instance;
 
 void main() {
@@ -26,25 +27,44 @@ void setupDependencies() {
   // Connection Bloc
   serviceLocator.registerSingleton(DiscoveryBloc());
 
-  // Effect Blocs
-  serviceLocator.registerLazySingleton(
-    () => EffectBloc(NoEffect()),
-  );
-  serviceLocator.registerLazySingleton(
-    () => StaticBloc(StaticEffect(color: Colors.black.toHSV())),
-  );
-  serviceLocator.registerLazySingleton(
-    () => BreathingBloc(BreathingEffect(
-        color: Colors.black.toHSV(), step: maxBreathingStep, peak: 1.0)),
-  );
-  serviceLocator.registerLazySingleton(
-    () => RainbowBloc(
-        RainbowEffect(saturation: 1.0, value: 1.0, step: maxRainbowStep)),
-  );
+  // Store service - to get saved effects
+  final storeService = StoreService();
+  serviceLocator.registerSingleton(storeService);
 
-  // Services
+  // Remaining services
   serviceLocator.registerSingleton(DiscoveryService());
   serviceLocator.registerSingleton(ControllerService());
+
+  // Effect Blocs
+  serviceLocator.registerSingleton(
+    EffectBloc(NoEffect()),
+  );
+  serviceLocator.registerSingletonAsync(
+    () async {
+      final savedStatic = await storeService.get(
+          defaultValue: StaticEffect(color: Colors.black.toHSV()));
+
+      return StaticBloc(savedStatic);
+    },
+  );
+  serviceLocator.registerSingletonAsync(
+    () async {
+      final savedBreathing = await storeService.get(
+          defaultValue: BreathingEffect(
+              color: Colors.black.toHSV(), step: maxBreathingStep, peak: 1.0));
+
+      return BreathingBloc(savedBreathing);
+    },
+  );
+  serviceLocator.registerSingletonAsync(
+    () async {
+      final savedRainbow = await storeService.get(
+          defaultValue:
+              RainbowEffect(saturation: 1.0, value: 1.0, step: maxRainbowStep));
+
+      return RainbowBloc(savedRainbow);
+    },
+  );
 }
 
 class BaseScreen extends StatelessWidget {
---
+++ app/lib/service/store_service.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:rusty_controller/main.dart';
+
+class StoreService {
+  /// Gets the store object in [defaultValue.name] if present and successful.
+  ///
+  /// Otherwise calls [save] with [defaultValue] and returns it.
+  Future<dynamic> get({required StorableObject defaultValue}) async {
+    try {
+      final value =
+          await _getRaw(defaultValue.storeName, defaultValue.fromJson);
+
+      if (value == null) {
+        log.i(
+            "No store for file '${defaultValue.storeName}', saving default provided.");
+        return await save(defaultValue);
+      }
+
+      log.v("Got file of store '${defaultValue.storeName}'");
+      return Future.value(value);
+    } on Exception catch (ex) {
+      log.w('Failed to get value for ${defaultValue.storeName}.', ex);
+      log.d('Writing default value provided.');
+      return await save(defaultValue);
+    }
+  }
+
+  // Saves, and returns [value] for convenience.
+  Future<StorableObject> save(StorableObject value) async {
+    final file = await _getStoreFile(value.storeName);
+    final jsonContent = jsonEncode(value.toJson());
+
+    await file.writeAsString(jsonContent);
+    log.v('Stored object to file ${file.path}');
+
+    return value;
+  }
+
+  /// Gets the store object in [storeName] mapping to [StorableObject]
+  /// via [fromJson], or null if the file doesn't exist.
+  Future<StorableObject?> _getRaw(String storeName,
+      StorableObject Function(Map<String, dynamic>) fromJson) async {
+    final file = await _getStoreFile(storeName);
+
+    final jsonContent = jsonDecode(await file.readAsString());
+
+    return fromJson(jsonContent);
+  }
+
+  Future<File> _getStoreFile(String storeName) async {
+    final dir = await getApplicationDocumentsDirectory();
+    final path =
+        "${dir.path}\\rusty_controller\\stores\\${storeName}_effect.json";
+    final file = File(path);
+
+    await file.create(recursive: true);
+
+    return file;
+  }
+}
+
+abstract class StorableObject {
+  String get storeName;
+
+  StorableObject fromJson(Map<String, dynamic> json);
+
+  Map<String, dynamic> toJson();
+}
