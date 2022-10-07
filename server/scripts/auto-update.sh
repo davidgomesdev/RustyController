4b0fe71947ac67efcaae4d989c1d2f7af9ab053a
David Gomes <LegendL3n@users.noreply.github.com>
2022-10-07 14:01:20 +0100

    Fix update failing when there are local changes

--- server/scripts/auto-update.sh
+++ server/scripts/auto-update.sh
@@ -20,11 +20,12 @@ cd "$HOME" || exit 1
 
 build () {
     echo "$INFO* Building...$RESET"
+    
+    git reset --hard || exit 1
 
     # This way we only pull the main branch
     git fetch origin main || exit 1
     git checkout FETCH_HEAD -B main || exit 1
-    git reset --hard || exit 1
     git pull || exit 1
     cd server/ || exit 1
 
