424b7b221ac602fa984c5578ae0964f7b56a6863
David Gomes <LegendL3n@users.noreply.github.com>
2023-02-19 16:46:39 +0000

    feat(server): log to file and stdout

--- server/src/tracing_setup.rs
+++ server/src/tracing_setup.rs
@@ -1,28 +1,19 @@
-use std::fs::File;
 use std::io;
-use std::sync::Mutex;
 
-use futures::SinkExt;
 use tracing::Level;
-use tracing::level_filters::LevelFilter;
-use tracing_subscriber::{fmt, Layer, Registry};
-use tracing_subscriber::filter::{filter_fn, FilterExt};
+use tracing_subscriber::{filter, fmt};
 use tracing_subscriber::fmt::writer::MakeWriterExt;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
 pub fn setup_tracing() {
-    Registry::default()
-        .with(
-            fmt::layer().with_writer(
-                io::stdout
-                    .with_max_level(Level::WARN)
-                    .or_else(io::stdout.with_filter(|metadata| {
-                        metadata
-                            .module_path()
-                            .map_or(false, |path| path.starts_with("rusty_controller"))
-                    }))
-            )
-        )
+    let file = tracing_appender::rolling::daily("logs", "daily");
+    let filter = filter::Targets::new()
+        .with_target("rusty_controller", Level::DEBUG)
+        .with_default(Level::INFO);
+
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_writer(io::stdout.and(file)))
+        .with(filter)
         .init();
 }
