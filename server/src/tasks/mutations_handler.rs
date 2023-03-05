949429c9c26af2b96acffefe539c945281fed3a8
David Gomes <LegendL3n@users.noreply.github.com>
2023-03-05 15:07:19 +0000

    feat(server): refactor mutations handler task

--- server/src/tasks/mutations_handler.rs
+++ server/src/tasks/mutations_handler.rs
@@ -2,72 +2,72 @@ use std::sync::Arc;
 use std::sync::Mutex;
 
 use tokio::sync::broadcast::Receiver;
-use tokio::task::JoinHandle;
 
 use crate::{EffectChange, EffectChangeType, EffectTarget};
 use crate::ps_move::controller::PsMoveController;
 use crate::spawn_tasks::InitialLedState;
 
-pub fn spawn(
+pub async fn run(
     controllers: Arc<Mutex<Vec<PsMoveController>>>,
     mut rx: Receiver<EffectChange>,
     initial_state: Arc<Mutex<InitialLedState>>,
-) -> JoinHandle<()> {
-    tokio::spawn(async move {
-        loop {
-            match rx.recv().await {
-                Ok(effect_change) => {
-                    let mut controllers = controllers.lock().unwrap();
-                    let target = effect_change.target;
-                    let effect = effect_change.effect;
+) {
+    loop {
+        match rx.recv().await {
+            Ok(effect_change) => {
+                let mut controllers = controllers.lock().unwrap();
+                let target = effect_change.target;
+                let effect = effect_change.effect;
 
-                    match target {
-                        EffectTarget::All => {
-                            tracing::info!("Setting effect '{effect}' for all controllers");
-                            controllers.iter_mut().for_each(|controller| {
-                                mutate_controller_effect(controller, effect);
-                                tracing::debug!("Controller '{}' set to {effect}", controller.bt_address);
-                            });
-
-                            if let EffectChangeType::Led { effect } = effect {
-                                let mut initial_state = initial_state.lock().unwrap();
-                                *initial_state = InitialLedState::from(effect);
-                                tracing::debug!("Set '{effect}' as initial effect.");
-                            }
-                        }
-                        EffectTarget::Only { bt_addresses } => {
+                match target {
+                    EffectTarget::All => {
+                        tracing::info!("Setting effect '{effect}' for all controllers");
+                        controllers.iter_mut().for_each(|controller| {
+                            mutate_controller_effect(controller, effect);
                             tracing::debug!(
-                                "Setting effect '{effect}' for {} controllers only",
-                                bt_addresses.len()
+                                "Controller '{}' set to {effect}",
+                                controller.bt_address
                             );
-                            bt_addresses.iter().for_each(|bt_address| {
-                                controllers
-                                    .iter_mut()
-                                    .find(|controller| controller.bt_address == *bt_address)
-                                    .map_or_else(
-                                        || {
-                                            tracing::warn!(
+                        });
+
+                        if let EffectChangeType::Led { effect } = effect {
+                            let mut initial_state = initial_state.lock().unwrap();
+                            *initial_state = InitialLedState::from(effect);
+                            tracing::debug!("Set '{effect}' as initial effect.");
+                        }
+                    }
+                    EffectTarget::Only { bt_addresses } => {
+                        tracing::debug!(
+                            "Setting effect '{effect}' for {} controllers only",
+                            bt_addresses.len()
+                        );
+                        bt_addresses.iter().for_each(|bt_address| {
+                            controllers
+                                .iter_mut()
+                                .find(|controller| controller.bt_address == *bt_address)
+                                .map_or_else(
+                                    || {
+                                        tracing::warn!(
                                         "The effect change had a non-existing controller! ('{bt_address}')"
                                     );
-                                        },
-                                        |controller| {
-                                            mutate_controller_effect(controller, effect);
-                                            tracing::info!(
+                                    },
+                                    |controller| {
+                                        mutate_controller_effect(controller, effect);
+                                        tracing::info!(
                                                 "Controller '{}' set to {effect}",
                                                 controller.bt_address
                                             );
-                                        },
-                                    );
-                            });
-                        }
+                                    },
+                                );
+                        });
                     }
                 }
-                Err(err) => {
-                    tracing::error!("Error occurred in receiving effect update. (Cause: {err})")
-                }
-            };
-        }
-    })
+            }
+            Err(err) => {
+                tracing::error!("Error occurred in receiving effect update. (Cause: {err})")
+            }
+        };
+    }
 }
 
 fn mutate_controller_effect(controller: &mut PsMoveController, effect: EffectChangeType) {
