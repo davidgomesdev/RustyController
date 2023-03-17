b9ab68799db1ecef5445540b4c42029265c7d260
David Gomes <LegendL3n@users.noreply.github.com>
2023-03-17 12:05:32 +0000

    chore: log metrics as micros rather than nanos

--- server/src/spawn_tasks.rs
+++ server/src/spawn_tasks.rs
@@ -126,9 +126,9 @@ fn log_metrics(name: &str, metrics: TaskMetrics) {
     tracing::info!(
         target: "rusty_controller::metrics",
         task = name,
-        poll_duration = metrics.mean_poll_duration().as_nanos(),
-        scheduled_duration = metrics.mean_scheduled_duration().as_nanos(),
-        idle_duration = metrics.mean_idle_duration().as_nanos(),
+        poll_duration = metrics.mean_poll_duration().as_micros(),
+        scheduled_duration = metrics.mean_scheduled_duration().as_micros(),
+        idle_duration = metrics.mean_idle_duration().as_micros(),
         "Durations: Poll {:.2?} / Scheduled {:.2?} / Idle {:.2?}",
         metrics.mean_poll_duration(),
         metrics.mean_scheduled_duration(),
