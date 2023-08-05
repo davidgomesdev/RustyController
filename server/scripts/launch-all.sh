df1dbcd391ceca98969cc117a89fd00092f788fe
David Gomes <LegendL3n@users.noreply.github.com>
2023-08-05 15:27:27 +0100

    chore: add logs in launch-all script

--- server/scripts/launch-all.sh
+++ server/scripts/launch-all.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Colors
+
+SUCCESS=$(tput setaf 2)
+RESET=$(tput sgr0)
+
 # Start node_exporter
 
 RUSTY_PATH=$(pwd)
@@ -13,14 +18,22 @@ fi
 
 # Start Grafana stack
 
+printf "Launching Grafana stack...\n\n"
+
 (cd server/docker && docker compose -f grafana.yaml up --wait -d) >> /var/log/rusty-controller/run-grafana-stack.log 2>&1
 
 # Update and launch server
+
+printf "Updating and launching server...\n\n"
 
 (cp server/scripts/auto-update.sh /tmp/rusty-auto-update.sh && bash /tmp/rusty-auto-update.sh) >> /var/log/rusty-controller/auto-update.log 2>&1
 
 # Update and launch non-adhoc plugins
 
+printf "Updating and launching plugins...\n\n"
+
 PLUGINS_PATH=${PLUGINS_PATH:-../RustyController-plugins}
 
 mkdir -p /var/log/rusty-controller/plugins/ && (cd "$PLUGINS_PATH" && git pull && bash run-all.sh) >> /var/log/rusty-controller/plugins/run-all.log 2>&1
+
+echo -e "${SUCCESS}Finished!$RESET"
