5a3056d5260547e91f6b828c2436013c3e362474
David Gomes <10091092+LegendL3n@users.noreply.github.com>
2023-09-22 21:11:54 +0100

    refactor: ditch files for logs (#55)
    * refactor: remove log files; get logs via Loki directly
    
    * feat: update dashboards
    
    ---------
    
    Co-authored-by: David Gomes <LegendL3n@users.noreply.github.com>

--- server/src/tracing_setup.rs
+++ server/src/tracing_setup.rs
@@ -1,18 +1,14 @@
 use std::io;
 
-use tokio::fs;
 use tracing::Level;
 use tracing_loki::BackgroundTask;
 use tracing_loki::url::Url;
 use tracing_subscriber::{filter, fmt};
-use tracing_subscriber::fmt::writer::MakeWriterExt;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use warp::hyper::Client;
 
 const LOKI_BASE_URL: &str = "http://127.0.0.1:3100";
-
-const LOGS_DIRECTORY: &str = "/var/log/rusty-controller/server";
 
 fn build_loki_layer() -> (tracing_loki::Layer, BackgroundTask) {
     tracing_loki::layer(
@@ -26,22 +22,13 @@ fn build_loki_layer() -> (tracing_loki::Layer, BackgroundTask) {
 }
 
 pub async fn setup_loki() {
-    let directory = fs::create_dir_all("/var/log/rusty-controller/server")
-        .await
-        .map(|_| {
-            tracing::info!("Failed to create directory in /var/log. Using ./logs");
-            LOGS_DIRECTORY
-        })
-        .unwrap_or("logs");
-
-    let file = tracing_appender::rolling::daily(directory, "daily");
     let filter = filter::Targets::new()
         .with_target("rusty_controller", Level::DEBUG)
         .with_default(Level::WARN);
 
     let registry = tracing_subscriber::registry().with(filter).with(
         fmt::layer()
-            .with_writer(io::stdout.and(file))
+            .with_writer(io::stdout)
     );
 
     let http = Client::new();
