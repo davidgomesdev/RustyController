9e2430c789fdf5c014716f6f610ca80ecd08f53b
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2023-11-09 22:02:15 +0000

    fix: tmux windows not correct

--- server/scripts/run-exporters.sh
+++ server/scripts/run-exporters.sh
@@ -3,13 +3,13 @@
 LOG_DIRECTORY="/var/log/rusty-controller/prometheus-exporters"
 mkdir -p "$LOG_DIRECTORY"
 
-tmux kill-session -t prometheus-exporters 2>/dev/null && echo "Killed existing Prometheus Exporters tmux session"
+tmux kill-session -t "Prometheus Exporters" 2>/dev/null && echo "Killed existing Prometheus Exporters tmux session"
 
 tmux new-session -d -s "Prometheus Exporters"
 
-tmux new-window -n "node_exporter" -c "$(pwd)" "./node_exporter 2>&1 | tee $LOG_DIRECTORY/node_exporter.log"
+tmux new-window -t "Prometheus Exporters" -n "node_exporter" -c "$(pwd)" "./node_exporter 2>&1 | tee $LOG_DIRECTORY/node_exporter.log"
 echo "Started new tmux session for node_exporter"
 
-tmux new-window -d -n "process-exporter" -c "$(pwd)" "process-exporter 2>&1 | tee $LOG_DIRECTORY/process-exporter.log"
+tmux new-window -t "Prometheus Exporters" -n "process-exporter" -c "$(pwd)" "process-exporter 2>&1 | tee $LOG_DIRECTORY/process-exporter.log"
 echo "Started new tmux session for process-exporter"
 
