12b8d863a403bcf655bc58092331afff90162094
David Gomes <10091092+davidgomesdev@users.noreply.github.com>
2023-11-09 21:59:43 +0000

    fix: script directory and remove leftover

--- server/scripts/run-exporters.sh
+++ server/scripts/run-exporters.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-mkdir -p /var/log/prometheus-exporters
+LOG_DIRECTORY="/var/log/rusty-controller/prometheus-exporters"
+mkdir -p "$LOG_DIRECTORY"
 
 tmux kill-session -t prometheus-exporters 2>/dev/null && echo "Killed existing Prometheus Exporters tmux session"
 
 tmux new-session -d -s "Prometheus Exporters"
 
-tmux new-window -n "node_exporter" -c "$(pwd)" './node_exporter 2>&1 | tee /var/log/prometheus-exporters/node_exporter.log'
+tmux new-window -n "node_exporter" -c "$(pwd)" "./node_exporter 2>&1 | tee $LOG_DIRECTORY/node_exporter.log"
 echo "Started new tmux session for node_exporter"
 
-tmux new-window -d -n "process-exporter" -c "$(pwd)" 'process-exporter 2>&1 | tee /var/log/prometheus-exporters/process-exporter.log'
+tmux new-window -d -n "process-exporter" -c "$(pwd)" "process-exporter 2>&1 | tee $LOG_DIRECTORY/process-exporter.log"
 echo "Started new tmux session for process-exporter"
 
